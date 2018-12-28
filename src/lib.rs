e18c2a3ffbcca65f55f5d0770f0f3347be0a2eaa
MurphyMarkW <mark@gudcodes.io>
2018-12-28 12:38:44 -0600

    add wrapping

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,17 @@
 
 extern crate test;
 
+
+// Operations like + on u32 values is intended to never overflow. This is
+// problematic for certain operations that depend on overflow and underflow,
+// such as the following bitwise arithmetic. We avoid the issue of overflow
+// and underflow protection by using the Wrapping type, which compiles to
+// efficient machine code, but maintains a safe interface.
+use std::num::Wrapping;
+
+
+const ONE: Wrapping<usize> = Wrapping(1 as usize);
+
 /// Converts the right-most 1 bit to a 0.
 /// Returns 0 if all bits are 0.
 ///
@@ -13,11 +24,23 @@ extern crate test;
 /// assert_eq!(format!("{:b}", y), "1001100");
 /// ```
 pub fn binary_turn_off_rightmost_one(x: usize) -> usize {
-    x & (x - 1)
+    let w = Wrapping(x);
+    x & (w - ONE).0
 }
 
+/// Converts the right-most 0 bit to a 1.
+/// Returns the value `std::usize::MAX` if all bits are 1.
+///
+/// ```
+/// # use delight::binary_turn_on_rightmost_zero;
+/// let x = usize::from_str_radix("1001111", 2).unwrap();
+/// let y = binary_turn_on_rightmost_zero(x);
+///
+/// assert_eq!(format!("{:b}", y), "1011111");
+/// ```
 pub fn binary_turn_on_rightmost_zero(x: usize) -> usize {
-    x | (x + 1)
+    let w = Wrapping(x);
+    x | (w + ONE).0
 }
 
 #[cfg(test)]
